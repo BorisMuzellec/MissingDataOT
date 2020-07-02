8c0a651fcb07c63fa78d0fa6e32b9624d6e71744
BorisMuzellec <BorisMuzellec@users.noreply.github.com>
2020-07-02 21:02:47 +0200

    Missing data mechanisms with numpy or pytorch outputs

--- utils.py
+++ utils.py
@@ -149,8 +149,9 @@ def MAR_mask(X, p, p_obs):
 
     Parameters
     ----------
-    X : torch.DoubleTensor or torch.cuda.DoubleTensor, shape (n, d)
-        Data for which missing values will be simulated.
+    X : torch.DoubleTensor or np.ndarray, shape (n, d)
+        Data for which missing values will be simulated. If a numpy array is provided,
+        it will be converted to a pytorch tensor.
 
     p : float
         Proportion of missing values to generate for variables which will have missing values.
@@ -160,13 +161,18 @@ def MAR_mask(X, p, p_obs):
 
     Returns
     -------
-    mask : torch.BoolTensor or torch.cuda.BoolTensor, shape (n, d)
+    mask : torch.BoolTensor or np.ndarray (depending on type of X)
         Mask of generated missing values (True if the value is missing).
 
     """
 
     n, d = X.shape
-    mask = torch.zeros(n, d).bool()
+
+    to_torch = torch.is_tensor(X) ## output a pytorch tensor, or a numpy array
+    if not to_torch:
+        X = torch.from_numpy(X)
+
+    mask = torch.zeros(n, d).bool() if to_torch else np.zeros((n, d)).astype(bool)
 
     d_obs = max(int(p_obs * d), 1) ## number of variables that will have no missing values (at least one variable)
     d_na = d - d_obs ## number of variables that will have missing values
@@ -192,43 +198,54 @@ def MAR_mask(X, p, p_obs):
 
 ##### Missing not at random ######
 
-def MNAR_mask_logistic(X, p, p_params):
+def MNAR_mask_logistic(X, p, p_params =.3, exclude_inputs=True):
     """
-    Missing not at random mechanism with a logistic masking model. First, a subset of variables is randomly selected to
-    be used as inputs in a logistic masking mechanism. The remaining variables have missing values according to a
-    logistic model with random weights, re-scaled so as to attain the desired proportion of missing values on those
-    variables, as in the MAR mechanism. The difference is that the input variables are then masked with a MCAR
-    mechanism, which makes part of the missingness effectively dependent on unobserved variables.
+    Missing not at random mechanism with a logistic masking model. It implements two mechanisms:
+    (i) Missing probabilities are selected with a logistic model, taking all variables as inputs. Hence, values that are
+    inputs can also be missing.
+    (ii) Variables are split into a set of intputs for a logistic model, and a set whose missing probabilities are
+    determined by the logistic model. Then inputs are then masked MCAR (hence, missing values from the second set will
+    depend on masked values.
+    In either case, weights are random and the intercept is selected to attain the desired proportion of missing values.
 
     Parameters
     ----------
-    X : torch.DoubleTensor or torch.cuda.DoubleTensor, shape (n, d)
+    X : torch.DoubleTensor or np.ndarray, shape (n, d)
         Data for which missing values will be simulated.
+        If a numpy array is provided, it will be converted to a pytorch tensor.
 
     p : float
         Proportion of missing values to generate for variables which will have missing values.
 
     p_params : float
-        Proportion of variables that will be used for the logistic masking model.
+        Proportion of variables that will be used for the logistic masking model (only if exclude_inputs).
+
+    exclude_inputs : boolean, default=True
+        True: mechanism (ii) is used, False: (i)
 
     Returns
     -------
-    mask : torch.BoolTensor or torch.cuda.BoolTensor, shape (n, d)
+    mask : torch.BoolTensor or np.ndarray (depending on type of X)
         Mask of generated missing values (True if the value is missing).
 
     """
 
     n, d = X.shape
-    mask = torch.zeros(n, d).bool()
 
-    d_params = max(int(p_params * d), 1) ## number of variables used as inputs
-    d_na = d - d_params ## number of variables masked with the logistic model
+    to_torch = torch.is_tensor(X) ## output a pytorch tensor, or a numpy array
+    if not to_torch:
+        X = torch.from_numpy(X)
+
+    mask = torch.zeros(n, d).bool() if to_torch else np.zeros((n, d)).astype(bool)
+
+    d_params = max(int(p_params * d), 1) if exclude_inputs else d ## number of variables used as inputs (at least 1)
+    d_na = d - d_params if exclude_inputs else d ## number of variables masked with the logistic model
 
     ### Sample variables that will be parameters for the logistic regression:
-    idxs_params = np.random.choice(d, d_params, replace=False) ### select at least one variable
-    idxs_nas = np.array([i for i in range(d) if i not in idxs_params])
+    idxs_params = np.random.choice(d, d_params, replace=False) if exclude_inputs else np.arange(d)
+    idxs_nas = np.array([i for i in range(d) if i not in idxs_params]) if exclude_inputs else np.arange(d)
 
-    ### Other variables will have NA proportions that depend on those observed variables, through a logistic model
+    ### Other variables will have NA proportions selected by a logistic model
     ### The parameters of this logistic model are random.
 
     ### Pick coefficients so that W^Tx has unit variance (avoids shrinking)
@@ -241,50 +258,55 @@ def MNAR_mask_logistic(X, p, p_params):
     ber = torch.rand(n, d_na)
     mask[:, idxs_nas] = ber < ps
 
-    ## Now, mask some values used in the logistic model at random
+    ## If the inputs of the logistic model are excluded from MNAR missingness,
+    ## mask some values used in the logistic model at random.
     ## This makes the missingness of other variables potentially dependent on masked values
-    mask[:, idxs_params] = torch.rand(n, d_params) < p
+
+    if exclude_inputs:
+        mask[:, idxs_params] = torch.rand(n, d_params) < p
 
     return mask
 
 def MNAR_self_mask_logistic(X, p):
     """
-    Missing not at random mechanism with a logistic self-masking model. All variables have missing values according to a
-    logistic model on *all* variables with random weights, re-scaled so as to attain the desired proportion of missing
-    values. Since missingness depends on all variables, it will depend on unobserved values as well.
+    Missing not at random mechanism with a logistic self-masking model. Variables have missing values probabilities
+    given by a logistic model, taking the same variable as input (hence, missingness is independent from one variable
+    to another). The intercepts are selected to attain the desired missing rate.
 
     Parameters
     ----------
-    X : torch.DoubleTensor or torch.cuda.DoubleTensor, shape (n, d)
+    X : torch.DoubleTensor or np.ndarray, shape (n, d)
         Data for which missing values will be simulated.
+        If a numpy array is provided, it will be converted to a pytorch tensor.
 
     p : float
         Proportion of missing values to generate for variables which will have missing values.
 
     Returns
     -------
-    mask : torch.BoolTensor or torch.cuda.BoolTensor, shape (n, d)
+    mask : torch.BoolTensor or np.ndarray (depending on type of X)
         Mask of generated missing values (True if the value is missing).
 
     """
 
     n, d = X.shape
-    mask = torch.zeros(n, d).bool()
+
+    to_torch = torch.is_tensor(X) ## output a pytorch tensor, or a numpy array
+    if not to_torch:
+        X = torch.from_numpy(X)
 
     ### Variables will have NA proportions that depend on those observed variables, through a logistic model
     ### The parameters of this logistic model are random.
 
     ### Pick coefficients so that W^Tx has unit variance (avoids shrinking)
-    coeffs = pick_coeffs(X, np.arange(d), np.arange(d))
+    coeffs = pick_coeffs(X, self_mask=True)
     ### Pick the intercepts to have a desired amount of missing values
-    intercepts = fit_intercepts(X, coeffs, p)
+    intercepts = fit_intercepts(X, coeffs, p, self_mask=True)
 
-    ps = torch.sigmoid(X.mm(coeffs) + intercepts)
-    ### Rescale to have a desired amount of missing values
-    ps /= (ps.mean(0) / p)
+    ps = torch.sigmoid(X * coeffs + intercepts)
 
-    ber = torch.rand(n, d)
-    mask = ber < ps
+    ber = torch.rand(n, d) if to_torch else np.random.rand(n, d)
+    mask = ber < ps if to_torch else ber < ps.numpy()
 
     return mask
 
@@ -297,8 +319,9 @@ def MNAR_mask_quantiles(X, p, q, p_params, cut='both', MCAR=False):
 
     Parameters
     ----------
-    X : torch.DoubleTensor or torch.cuda.DoubleTensor, shape (n, d)
+    X : torch.DoubleTensor or np.ndarray, shape (n, d)
         Data for which missing values will be simulated.
+        If a numpy array is provided, it will be converted to a pytorch tensor.
 
     p : float
         Proportion of missing values to generate for variables which will have missing values.
@@ -318,13 +341,17 @@ def MNAR_mask_quantiles(X, p, q, p_params, cut='both', MCAR=False):
         
     Returns
     -------
-    mask : torch.BoolTensor or torch.cuda.BoolTensor, shape (n, d)
+    mask : torch.BoolTensor or np.ndarray (depending on type of X)
         Mask of generated missing values (True if the value is missing).
 
     """
     n, d = X.shape
 
-    mask = torch.zeros(n, d).bool()
+    to_torch = torch.is_tensor(X) ## output a pytorch tensor, or a numpy array
+    if not to_torch:
+        X = torch.from_numpy(X)
+
+    mask = torch.zeros(n, d).bool() if to_torch else np.zeros((n, d)).astype(bool)
 
     d_na = max(int(p_params * d), 1) ## number of variables that will have NMAR values
 
@@ -333,14 +360,14 @@ def MNAR_mask_quantiles(X, p, q, p_params, cut='both', MCAR=False):
 
     ### check if values are greater/smaller that corresponding quantiles
     if cut == 'upper':
-        quants = quantile(X[:, idxs_na], q, dim=0)
+        quants = quantile(X[:, idxs_na], 1-q, dim=0)
         m = X[:, idxs_na] >= quants
     elif cut == 'lower':
-        quants = quantile(X[:, idxs_na], 1-q, dim=0)
+        quants = quantile(X[:, idxs_na], q, dim=0)
         m = X[:, idxs_na] <= quants
     elif cut == 'both':
-        l_quants = quantile(X[:, idxs_na], 1-q, dim=0)
-        u_quants = quantile(X[:, idxs_na], q, dim=0)
+        u_quants = quantile(X[:, idxs_na], 1-q, dim=0)
+        l_quants = quantile(X[:, idxs_na], q, dim=0)
         m = (X[:, idxs_na] <= l_quants) | (X[:, idxs_na] >= u_quants)
 
     ### Hide some values exceeding quantiles
@@ -354,22 +381,35 @@ def MNAR_mask_quantiles(X, p, q, p_params, cut='both', MCAR=False):
     return mask
 
 
-def pick_coeffs(X, idxs_obs, idxs_nas):
+def pick_coeffs(X, idxs_obs=None, idxs_nas=None, self_mask=False):
     n, d = X.shape
-    d_obs = len(idxs_obs)
-    d_na = len(idxs_nas)
-    coeffs = torch.randn(d_obs, d_na)
-    Wx = X[:, idxs_obs].mm(coeffs)
-    coeffs /= torch.std(Wx, 0, keepdim=True)
+    if self_mask:
+        coeffs = torch.randn(d)
+        Wx = X * coeffs
+        coeffs /= torch.std(Wx, 0)
+    else:
+        d_obs = len(idxs_obs)
+        d_na = len(idxs_nas)
+        coeffs = torch.randn(d_obs, d_na)
+        Wx = X[:, idxs_obs].mm(coeffs)
+        coeffs /= torch.std(Wx, 0, keepdim=True)
     return coeffs
 
 
-def fit_intercepts(X, coeffs, p):
-    d_obs, d_na = coeffs.shape
-    intercepts = torch.zeros(d_na)
-    for j in range(d_na):
-        def f(x):
-            return torch.sigmoid(X.mv(coeffs[:, j]) + x).mean().item() - p
-        intercepts[j] = optimize.bisect(f, -50, 50)
+def fit_intercepts(X, coeffs, p, self_mask=False):
+    if self_mask:
+        d = len(coeffs)
+        intercepts = torch.zeros(d)
+        for j in range(d):
+            def f(x):
+                return torch.sigmoid(X * coeffs[j] + x).mean().item() - p
+            intercepts[j] = optimize.bisect(f, -50, 50)
+    else:
+        d_obs, d_na = coeffs.shape
+        intercepts = torch.zeros(d_na)
+        for j in range(d_na):
+            def f(x):
+                return torch.sigmoid(X.mv(coeffs[:, j]) + x).mean().item() - p
+            intercepts[j] = optimize.bisect(f, -50, 50)
     return intercepts
 
